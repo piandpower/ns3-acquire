5e0b5d7dee470ca5333ca274bd50b7a506375134
Sebastian <sebastianlozano22@gmail.com>
2016-11-10 21:29:43 -0500

    tuff :wq :

--- acquire/app.py
+++ acquire/app.py
@@ -3,6 +3,10 @@ from flask import Flask, request, render_template, jsonify
 from flask_restplus import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 import requests
+import lxml
+from lxml import html
+import BeautifulSoup
+from BeautifulSoup import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -43,3 +47,35 @@ class Refresh(Resource):
 
     def post(self):
         ''' refresh db '''
+        return self.pullBodyOfURL("http://www.nytimes.com/2016/11/08/science/bats-wind-power-turbines.html");
+         
+    
+    def topNArticles(self, topic, n): 
+        urlString = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
+        #to do: add custom topic
+        urlString = urlString + "?q=wind+power" + "&api-key=61f3909960f048909642771cedab3b76" + "&response-format=jsonp" + "&callback=svc_search_v2_articlesearch"
+        data = '{"query":{"bool":{"must":[{"text":{"record.document":"SOME_JOURNAL"}},{"text":{"record.articleTitle":"farmers"}}],"must_not":[],"should":[]}},"from":0,"size":50,"sort":[],"facets":{}}'
+        response = requests.get(urlString, data = data)
+        docs = response.json()["docs"]
+        
+        for i in range(0, len(docs)):
+            doc = docs[i];
+            article_url = doc["web_url"]
+
+    
+    def pullBodyOfURL(self, url):
+        page = requests.get(url)
+        html_content = page.text
+        soup = BeautifulSoup(html_content, 'lxml')
+        storyList = soup.find_all('p', 'story-body-text story-content')
+        storyText = " ".join(storyList) #join by space
+        return storyText
+
+
+
+        #tree = html.fromstring(page.content)
+        #storyList = tree.xpath('//p[@class="story-body-text story-content"]/text()')
+        #storyText = " ".join(storyList) #join by space
+        #return storyText
+
+
