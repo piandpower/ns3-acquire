615438bae13fb2622b1d13be7c7abedefe2c7908
Sebastian <sebastianlozano22@gmail.com>
2017-01-09 18:54:39 -0500

    full guardian + NYT functionality

--- acquire/app.py
+++ acquire/app.py
@@ -17,6 +17,7 @@ ROOT_URL = os.getenv('ROOT_URL', 'localhost')
 VERSION_NO = os.getenv('VERSION_NO', '1.0')
 APP_NAME = os.getenv('APP_NAME', "Devil's Advocate")
 GUAR_KEY = os.getenv('GUAR_KEY')
+NYT_KEY = os.getenv('NYT_KEY')
 DEBUG = os.getenv('DEBUG', False)
 api = Api(app, version=VERSION_NO, title=APP_NAME)
 public_ns = api.namespace('Public', description='Public methods')
@@ -46,32 +47,36 @@ class ArticleRoute(Resource):
 
     def get(self, topic):
         filtered = Article.query.filter_by(topic=topic).all()
-        return [article.body for article in filtered]
+        return [article.url for article in filtered]
 
 @public_ns.route('/refresh')
 class Refresh(Resource):
 
     def post(self):
         ''' refresh db '''
-        return self.getGuardianArticles("wind power")
-        # return self.topNArticles('wind power', 10)
-        # return self.pullBodyOfURL("http://www.nytimes.com/2016/11/08/science/bats-wind-power-turbines.html");
+        #return self.getGuardianArticles("wind power")
+        return self.topNArticles('wind power', 20)
+        #return self.pullBodyOfURL("http://www.nytimes.com/2016/11/08/science/bats-wind-power-turbines.html");
          
+
+
     
     def topNArticles(self, topic, n): 
-        urlString = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
-        urlString = urlString + "?q=" + topic.replace(' ', '+') + "&api-key=61f3909960f048909642771cedab3b76" + "&response-format=jsonp" + "&callback=svc_search_v2_articlesearch"
-        # data = '{"query":{"bool":{"must":[{"text":{"record.document":"SOME_JOURNAL"}},{"text":{"record.articleTitle":"farmers"}}],"must_not":[],"should":[]}},"from":0,"size":50,"sort":[],"facets":{}}'
-        response = requests.get(urlString)
-        docs = response.json()['response']['docs']
-        articles = []
-        for doc in docs:
-            article_url = doc["web_url"]
-            text = self.pullBodyOfURL(article_url)
-            new_article = Article(topic, text, article_url)
+        NYT_urls = self.getNYTArticles(topic)
+        GUAR_urls = self.getGuardianArticles(topic)
+
+        NYT_bodies = self.pullBodyFromURLSet(NYT_urls, "new york times")
+        GUAR_bodies = self.pullBodyFromURLSet(GUAR_urls, "guardian")
+        
+        urls = NYT_urls + GUAR_urls
+        bodies = NYT_bodies + GUAR_bodies
+
+        for (article_url, body) in zip(urls, bodies):
+            new_article = Article(topic, body, article_url)
             if not Article.query.filter_by(url=article_url).first():
                 db.session.add(new_article)
         db.session.commit()
+
         return [article.url for article in Article.query.filter_by(topic=topic).limit(n)]
     
     def get_contents(self, tag, arr):
@@ -84,16 +89,22 @@ class Refresh(Resource):
         else:
             arr.append(tag.string)
 
-    def pullBodyOfURL(self, url):
+    def pullBodyOfURL(self, url, source):
         page = requests.get(url)
         html_content = page.text
         soup = BeautifulSoup(html_content, 'lxml')
-        tags = [tag for tag in soup.find_all('p', 'story-body-text story-content')]
+        tags = [tag for tag in soup.find_all('p', self.classTagFromSource(source))]
         strings = []
         for tag in tags:
             self.get_contents(tag, strings)
         storyText = " ".join(strings)
         return storyText
+
+    def classTagFromSource(self, source):
+        if source == "new york times":
+            return 'story-body-text story-content'
+        elif source == "guardian":
+            return 'content__article-body from-content-api js-article__body'
 
     def getGuardianArticles(self, topic):
         header = {
@@ -104,5 +115,21 @@ class Refresh(Resource):
         content_articles_data = content_articles.get_content_response()
         results = content_articles.get_results(content_articles_data)
 
-        webTitles = [result["webUrl"] for result in results]
-        return ("{title}" .format(title=webTitles))
+        webUrls = [result["webUrl"] for result in results]
+        return webUrls
+
+    def getNYTArticles(self, topic):
+        urlString = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
+        urlString = urlString + "?q=" + topic.replace(' ', '+') + "&api-key=" + NYT_KEY + "&response-format=jsonp" + "&callback=svc_search_v2_articlesearch"
+        
+        response = requests.get(urlString)
+        docs = response.json()['response']['docs']
+        webUrls = [doc["web_url"] for doc in docs]
+        return webUrls
+
+    def pullBodyFromURLSet(self, urlSet, source):
+        bodyList = [self.pullBodyOfURL(url, source) for url in urlSet]
+        return bodyList
+        
+        
+
